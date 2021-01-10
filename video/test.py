9141e216945d9885aa99443cdb3beea88a032a8a
When <BalanceUhen@gmail.com>
2021-01-10 21:58:50 +0800

    最终更新

--- video/test.py
+++
@@ -1,7 +0,0 @@
-from manim import *
-
-
-class HappyTest(Scene):
-    def construct(self):
-        happy = SVGMobject(r"E:\github\Differential-restraint-system\video\asset\happy.svg")
-        self.play(ShowCreation(happy))
